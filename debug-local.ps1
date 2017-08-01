eb08a1c86e1b8d7de06d480f26e58a0d0dce25fe
Erb Felix (FICMD) ext <a7htb09@eur.corp.vattenfall.com>
2017-08-01 18:42:14 +0200

    Added new functionality to checkpoint module

--- debug-local.ps1
+++ debug-local.ps1
@@ -1,18 +1,79 @@
 #Requires -Version 4
 $userCredential = [PSCredential]::new(
     'apiadmin',
-    (ConvertTo-SecureString -String 'password' -AsPlainText -Force)
+    (ConvertTo-SecureString -String 'RHrMU84*0d99' -AsPlainText -Force)
 )
-Get-Module -Name vfCheckpointFirewall | Remove-Module
-Import-Module -Name .\vfCheckpointFirewall.psd1
 
+$baseScriptPath = $psScriptRoot
+if (($psEditor -ne $null) -and ([string]::IsNullOrEmpty($baseScriptPath)))
+{
+    $baseScriptPath = ([Io.FileInfo]$psEditor.GetEditorContext().CurrentFile.Path).Directory.FullName
+}
+
+Get-Module -Name vfCheckpointFirewall | Remove-Module
+Import-Module -Name $baseScriptPath\vfCheckpointFirewall.psd1
 
 $HostName = "10.221.4.6"
+Connect-ckpSession -HostName $HostName -Credential $userCredential
 
-Register-ckpSession -HostName $HostName -Credential $userCredential
+$networks = Get-ckpNetwork -Limit 500 -GetAll
 
-$d = Get-ckpNetwork
 
-$d.objects[0].name
+$azurePath = Join-Path -Path $env:USERPROFILE -ChildPath "desktop/PublicIPs_20170731.xml"
+[xml]$azureXml = Get-Content -Path $azurePath
+$nets = @()
+$azureXml.AzurePublicIpAddresses.Region | Where-Object {$_.Name -ilike 'europe*'} | Foreach-Object {
+    $region = $_
+    Foreach ($range in $region.IpRange)
+    {
+        $match = $range.Subnet | Select-String -Pattern '(\d{1,4}\.\d{1,4}\.\d{1,4}\.\d{1,4})\/(\d{2})?'
+        $nets += [PsCustomObject]@{
+            name       = "azure_public_$($region.name)_$($match.matches.Groups[1].value)"
+            Subnet     = $match.matches.Groups[1].value
+            MaskLength = $match.matches.Groups[2].value
+        }
+    }
+}
 
-Get-ckpNetwork -NetworkName $d.objects[0].name
+$missingNetworks = Compare-Object -ReferenceObject $nets -DifferenceObject (
+    $networks | Select-Object name, @{n = "Subnet";e = {$_subnet4}}, @{n = "MaskLenght";e = {$_.'mask-length4'}}
+)
+$missingNetworks.Count
+
+Foreach ($missingNet in $missingNetworks)
+{
+    $missingNet.InputObject | Add-ckpNetwork
+}
+
+$groupName = 'azure_public_we'
+$group = Get-ckpGroup -Name $groupName
+
+if (-Not $group)
+{
+    $member = $networks | Where-Object {$_.name -ilike 'azure_public_europewest*'} | Select-Object -ExpandProperty uid
+    Add-ckpGroup -Name $groupName -Member $member
+}
+$group.members.Count
+
+
+Undo-ckpSession
+
+Publish-ckpSession
+
+Disconnect-ckpSession
+
+
+$lastNetwork = $networks[-1]
+
+$lastNetwork
+
+$member = $group.members.uid
+$newMember = $member[1..$($member.count - 1)]
+$lastMember = $member[-1]
+
+Set-ckpGroup -Name $groupName -Member $newMember
+
+
+Remove-ckpNetwork -Uid $lastNetwork.uid
+
+#Invoke-WebRequest -Uri 'https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/' -OutFile "huhu.xml"
--- vfCheckpointFirewall.psm1
+++ vfCheckpointFirewall.psm1
@@ -1,5 +1,6 @@
 [string] $Script:HostName = $null
 [string] $Script:SessionID = $null
+[string] $Script:SessionUID = $null
 
 Function Invoke-ckpWebRequest
 {
@@ -25,7 +26,20 @@ Function Invoke-ckpWebRequest
         [ValidateNotNullOrEmpty()]
         [PSCredential] $Credential
     )
-    [Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+    #[Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+    Add-Type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
 
     $requestUri = [System.UriBuilder] @{
         Host   = $HostName
@@ -60,11 +74,27 @@ Function Invoke-ckpWebRequest
         $jsonBody = ConvertTo-Json -Depth 99 -InputObject $Body
         $requestParams['Body'] = $jsonBody
         Write-Debug "JsonBody: `n $jsonBody"
+    }else{
+        $requestParams['Body'] = '{}'
     }
     Write-Debug ($requestParams | Format-Table -AutoSize | Out-String).Trim()
     Write-Debug "REST CALL: '$($requestUri.ToString())'"
-    $response = Invoke-RestMethod @requestParams -Verbose:$VerbosePreference -Debug:$DebugPreference
-    return $response
+    try
+    {
+        return Invoke-RestMethod @requestParams -Verbose:$VerbosePreference -Debug:$DebugPreference
+    }
+    catch [System.Net.WebException]
+    {
+        $httpError = $_
+        if ($httpError.Exception.Response.StatusCode -eq [Net.HttpStatusCode]::NotFound)
+        {
+            return $null
+        }
+        else
+        {
+            throw $httpError
+        }
+    }
 }
 
 Function Get-ckpInternalSession
@@ -75,20 +105,35 @@ Function Get-ckpInternalSession
     }
 
     return @{
-        HostName  = $Script:HostName
-        SessionID = $Script:SessionID
+        HostName   = $Script:HostName
+        SessionID  = $Script:SessionID
+        SessionUID = $Script:SessionUID
     }
 }
 
 Function Get-ckpSession
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Generic')]
     Param(
+        [Parameter(ParameterSetName = 'UID')]
+        [string] $UID
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateNotNull()]
+        [int] $Offset
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateRange(1,500)]
+        [int] $Limit
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [switch] $GetAll
 
     )
+    return Get-internalObject @PSBoundParameters -CommandSingularName 'session' -CommandPluralName 'sessions'
 }
 
-Function Register-ckpSession
+Function Connect-ckpSession
 {
     [CmdletBinding()]
     Param(
@@ -103,24 +148,178 @@ Function Register-ckpSession
 
     $requestParams = @{
         HostName   = $HostName
-        Method     = 'POST'
-        Commands   = @('login')
+        Command    = 'login'
         Credential = $Credential
     }
     $response = Invoke-ckpWebRequest @requestParams
     $Script:HostName = $HostName
     $Script:SessionID = $response.sid
+    $Script:SessionUID = $response.uid
+    $Script:SessionStartTime = (Get-Date)
     return $response
 }
 
-Function Unregister-ckpSession
+Function Disconnect-ckpSession
 {
     [CmdletBinding()]
+    Param()
+
+    $session = Get-ckpInternalSession
+    if (-Not $session)
+    {
+        return
+    }
+
+    $requestParams = @{
+        HostName  = $session.HostName
+        Command   = 'logout'
+        SessionID = $session.SessionID
+    }
+    $response = Invoke-ckpWebRequest @requestParams
+    return $response
+}
+
+Function Publish-ckpSession
+{
+    [CmdletBinding()]
+    Param()
+
+    $session = Get-ckpInternalSession
+    if (-Not $session)
+    {
+        return
+    }
+
+    $requestParams = @{
+        HostName  = $session.HostName
+        Command   = 'publish'
+        SessionID = $session.SessionID
+    }
+    return Invoke-ckpWebRequest @requestParams
+}
+
+Function Undo-ckpSession
+{
+    [CmdletBinding()]
+    Param()
+
+    $session = Get-ckpInternalSession
+    if (-Not $session)
+    {
+        return
+    }
+
+    $requestParams = @{
+        HostName  = $session.HostName
+        Command   = 'discard'
+        SessionID = $session.SessionID
+    }
+    return Invoke-ckpWebRequest @requestParams
+}
+
+
+Function Get-internalObject
+{
+    [CmdletBinding(DefaultParameterSetName = 'Generic')]
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $CommandSingularName
+
+        ,[Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string] $CommandPluralName
+
+        ,[Parameter(ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
+
+        ,[Parameter(ParameterSetName = 'UID')]
+        [string] $UID
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateNotNull()]
+        [int] $Offset
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateRange(1,500)]
+        [int] $Limit
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [switch] $GetAll
+    )
+
+    $session = Get-ckpInternalSession
+    if (-Not $session)
+    {
+        throw "You are not logged in please run 'Connect-ckpSession'"
+    }
+    $command = "show-$($CommandPluralName.ToLower())"
+    $body = @{
+        limit = 100
+    }
+
+    if ($PSBoundParameters.Count -gt 0)
+    {
+        if ($PSCmdlet.ParameterSetName -ne 'Generic')
+        {
+            $command = "show-$($CommandSingularName.ToLower())"
+            Foreach ($boundParam in $PSBoundParameters.GetEnumerator())
+            {
+                if ($boundParam.Key -notin @('Name','UID'))
+                {
+                    continue
+                }
+                $body = @{
+                    "$(([string]$boundParam.Key).ToLower())" = $boundParam.Value
+                }
+            }
+        }
+        else
+        {
+            if ($Offset -ne $null)
+            {
+                $body['offset'] = $Offset
+            }
+            if ($Limit -ne $null)
+            {
+                $body['limit'] = $Limit
+            }
+        }
+    }
+
+    $requestParams = @{
+        HostName  = $session.HostName
+        Command   = $command
+        SessionID = $session.SessionID
+        Body      = $body
+    }
+    $response = Invoke-ckpWebRequest @requestParams
+    $returnValue = $response
+    if (($response -ne $null) -and (($response | Get-Member -MemberType NoteProperty -Name objects) -ne $null))
+    {
+        $returnValue = $response.objects
+        while (
+            ($GetAll) -and
+            (
+                (($Limit -ne $null) -and ($response.objects.Count -ge $Limit)) -or
+                (($Limit -eq $null) -and ($response.objects.Count -ge 50))
+            )
+        )
+        {
+            $requestParams['Body']['offset'] = $response.objects.Count
+            $requestParams['Body']['limit'] = 500
+            $Limit = 500
+            $response = Invoke-ckpWebRequest @requestParams
+            $returnValue += $response.objects
+        }
+    }
+    return $returnValue
 }
 
 Function Get-ckpNetwork
 {
-    [CmdletBinding(DefaultParameterSetName = 'Name')]
+    [CmdletBinding(DefaultParameterSetName = 'Generic')]
     Param(
         [Parameter(ParameterSetName = 'Name')]
         [ValidateNotNullOrEmpty()]
@@ -128,34 +327,290 @@ Function Get-ckpNetwork
 
         ,[Parameter(ParameterSetName = 'UID')]
         [string] $UID
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateNotNull()]
+        [int] $Offset
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateRange(1,500)]
+        [int] $Limit
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [switch] $GetAll
+
     )
+    return Get-internalObject @PSBoundParameters -CommandSingularName 'network' -CommandPluralName 'networks'
+}
 
-    $session = Get-ckpInternalSession
-    if (-Not $session)
-    {
-        throw "You are not logged in please run 'Register-ckpSession'"
-    }
-    $command = 'show-networks'
-    $body = @{
-        limit = 100
-    }
+Function Add-ckpNetwork
+{
+    [CmdletBinding(DefaultParameterSetName = 'Generic')]
+    Param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
 
-    if (-Not([string]::IsNullOrEmpty($Name)))
+        ,[Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Generic')]
+        [string] $Subnet
+
+        ,[Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Generic')]
+        [string] $MaskLength
+
+        ,[Parameter(Mandatory, ParameterSetName = 'v4')]
+        [string] $SubentV4
+
+        ,[Parameter(Mandatory, ParameterSetName = 'v4')]
+        [string] $MaskLengthV4
+
+        ,[Parameter(Mandatory, ParameterSetName = 'v6')]
+        [string] $SubentV6
+
+        ,[Parameter(Mandatory, ParameterSetName = 'v6')]
+        [string] $MaskLengthV6
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [Parameter(ParameterSetName = 'v4')]
+        [Parameter(ParameterSetName = 'v6')]
+        [string[]] $Tags
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [Parameter(ParameterSetName = 'v4')]
+        [Parameter(ParameterSetName = 'v6')]
+        [ValidateSet('disallow','allow')]
+        [string] $Broadcast
+    )
+    Begin
     {
-        $command = 'show-network'
-        $body = @{
-            name = $Name
+        $session = Get-ckpInternalSession
+        if (-Not $session)
+        {
+            throw "You are not logged in please run 'Connect-ckpSession'"
         }
     }
 
-    $requestParams = @{
-        HostName  = $HostName
-        Command   = $command
-        SessionID = $SessionID
-        Body      = $body
+    Process
+    {
+        $body = @{
+            name = $name
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'Generic')
+        {
+            $body += @{
+                subnet        = $Subnet
+                "mask-length" = $MaskLength
+            }
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'v4')
+        {
+            $body += @{
+                subnet4        = $SubnetV4
+                "mask-length4" = $MaskLengthV4
+            }
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'v6')
+        {
+            $body += @{
+                subnet6        = $SubnetV6
+                "mask-length6" = $MaskLengthV6
+            }
+        }
+
+        if (-Not ([string]::IsNullOrEmpty($Broadcast)))
+        {
+            $body['broadcast'] = $Broadcast
+        }
+
+        if (($Tags -ne $null) -and ($Tags.Count -gt 0))
+        {
+            $body['tags'] = $Tags
+        }
+
+        $requestParams = @{
+            HostName  = $session.HostName
+            Command   = 'add-network'
+            SessionID = $session.SessionID
+            Body      = $body
+        }
+        return Invoke-ckpWebRequest @requestParams
     }
-    $networks = Invoke-ckpWebRequest @requestParams
-    return $networks
 }
 
+Function Remove-ckpNetwork
+{
+    [CmdletBinding(DefaultParameterSetName = 'Name')]
+    Param(
+        [Parameter(Mandatory, ParameterSetName = 'Name', ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
 
+        ,[Parameter(Mandatory, ParameterSetName = 'Uid')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Uid
+    )
+
+    Begin
+    {
+        $session = Get-ckpInternalSession
+        if (-Not $session)
+        {
+            throw "You are not logged in please run 'Connect-ckpSession'"
+        }
+    }
+
+    Process
+    {
+        $body = @{
+           "$($PSCmdlet.ParameterSetName.ToLower())" = (Get-Variable -Name $($PSCmdlet.ParameterSetName) -ValueOnly)
+        }
+
+        $requestParams = @{
+            HostName  = $session.HostName
+            Command   = 'delete-network'
+            SessionID = $session.SessionID
+            Body      = $body
+        }
+        return Invoke-ckpWebRequest @requestParams
+    }
+}
+
+Function Get-ckpGroup
+{
+    [CmdletBinding(DefaultParameterSetName = 'Generic')]
+    Param(
+        [Parameter(ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
+
+        ,[Parameter(ParameterSetName = 'UID')]
+        [string] $UID
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [ValidateNotNull()]
+        [int] $Offset
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+         [ValidateRange(1,500)]
+        [int] $Limit
+
+        ,[Parameter(ParameterSetName = 'Generic')]
+        [switch] $GetAll
+    )
+
+    return Get-internalObject @PSBoundParameters -CommandSingularName 'group' -CommandPluralName 'groups'
+}
+
+Function Add-ckpGroup
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
+
+        ,[Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNull()]
+        [string[]] $Member
+
+        ,[Parameter()]
+        [ValidateNotNull()]
+        [string[]] $Tags
+    )
+    Begin
+    {
+        $session = Get-ckpInternalSession
+        if (-Not $session)
+        {
+            throw "You are not logged in please run 'Connect-ckpSession'"
+        }
+    }
+
+    Process
+    {
+        $body = @{
+            name = $Name
+            members = $Member
+        }
+
+        if (($Tags -ne $null) -and ($Tags.Count -gt 0))
+        {
+            $body['tags'] = $Tags
+        }
+
+        $requestParams = @{
+            HostName  = $session.HostName
+            Command   = 'add-group'
+            SessionID = $session.SessionID
+            Body      = $body
+        }
+        return Invoke-ckpWebRequest @requestParams
+    }
+}
+
+Function Set-ckpGroup
+{
+    [CmdletBinding(DefaultParameterSetName = 'Name')]
+    Param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Name')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Name
+
+       ,[Parameter(Mandatory, ParameterSetName = 'Uid')]
+        [ValidateNotNullOrEmpty()]
+        [string] $Uid
+
+       ,[Parameter(ValueFromPipelineByPropertyName, ParameterSetName = 'Name')]
+        [Parameter(ParameterSetName = 'Uid')]
+        [ValidateNotNull()]
+        [string[]] $Member
+
+       ,[Parameter(ParameterSetName = 'Name')]
+        [Parameter(ParameterSetName = 'Uid')]
+        [ValidateNotNull()]
+        [string] $NewName
+
+        ,[Parameter(ParameterSetName = 'Name')]
+        [Parameter(ParameterSetName = 'Uid')]
+        [ValidateNotNull()]
+        [string[]] $Tags
+    )
+    Begin
+    {
+        $session = Get-ckpInternalSession
+        if (-Not $session)
+        {
+            throw "You are not logged in please run 'Connect-ckpSession'"
+        }
+    }
+
+    Process
+    {
+        $body = @{
+            "$($PSCmdlet.ParameterSetName.ToLower())" = (Get-Variable -Name $($PSCmdlet.ParameterSetName) -ValueOnly)
+        }
+
+        if ($Member -ne $null)
+        {
+            $body['members'] = $Member
+        }
+
+        if (-Not ([string]::IsNullOrEmpty($NewName)))
+        {
+            $body['new-name'] = $NewName
+        }
+
+        if (($Tags -ne $null) -and ($Tags.Count -gt 0))
+        {
+            $body['tags'] = $Tags
+        }
+
+        $requestParams = @{
+            HostName  = $session.HostName
+            Command   = 'set-group'
+            SessionID = $session.SessionID
+            Body      = $body
+        }
+        return Invoke-ckpWebRequest @requestParams
+    }
+}
